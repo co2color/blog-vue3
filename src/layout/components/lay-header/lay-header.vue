7959842075423172f550658ce62cc4925ab540b1
co2color <819109255@qq.com>
2024-09-23 23:14:30 +0800

    feat: lay header

--- src/layout/components/lay-header/lay-header.vue
+++ src/layout/components/lay-header/lay-header.vue
@@ -24,12 +24,15 @@ const commandList = ref([
 </script>
 
 <template>
-  <header class="flex justify-between">
+  <header class="flex justify-between h-15 items-center text-coolgray">
     <span>cococolor</span>
-    <nav>
-      <a v-for="item in commandList" :key="item.label" class="inline-block" :href="item?.href" :target="item.href && '_blank'">
+    <nav class="grid gap-5 grid-auto-flow-col">
+      <a
+        v-for="item in commandList" :key="item.label" class="inline-block decoration-none text-coolgray transition-all
+        opacity-60 hover:opacity-100 cursor-pointer" :href="item?.href" :target="item.href && '_blank'"
+      >
         <div v-if="item.iconfont">icon</div>
-        <div v-else>{{ item.label }}</div>
+        <div v-else class="text-inherit">{{ item.label }}</div>
       </a>
     </nav>
   </header>

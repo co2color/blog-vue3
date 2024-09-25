de29f7c767c47bc176cbf89fc1d077647b77453b
co2color <819109255@qq.com>
2024-09-25 23:59:42 +0800

    feat

--- src/components/ToggleTheme/ToggleTheme.vue
+++ src/components/ToggleTheme/ToggleTheme.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
+import { useDark, useToggle } from '@vueuse/core'
 
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <div i-ri-moon-line dark:i-ri-moon-line />
+  <div i-ri-moon-line dark:i-ri-moon-line @click="toggleDark" />
 </template>

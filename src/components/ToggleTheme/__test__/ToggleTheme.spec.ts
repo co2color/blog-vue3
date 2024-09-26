7be7488d0c4f516563d9b2f3823828459628a17e
co2color <819109255@qq.com>
2024-09-27 00:37:35 +0800

    feat: todo : toggletheme test

---
+++ src/components/ToggleTheme/__test__/ToggleTheme.spec.ts
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils'
+
+import { describe, expect, it } from 'vitest'
+import ToggleTheme from '../ToggleTheme.vue'
+
+describe('toggle Theme', () => {
+  // it('renders properly', () => {
+  //   const wrapper = mount(ToggleTheme)
+  //   expect(wrapper.text()).toContain('Toggle Theme')
+  // })
+})

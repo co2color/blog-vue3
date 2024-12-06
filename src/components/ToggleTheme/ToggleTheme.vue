aefa77a9e827b3d3a9f17eb1548ecbe21b2b4658
cococolor <819109255@qq.com>
2024-12-06 18:55:11 +0800

    feat: startViewTransition type

--- src/components/ToggleTheme/ToggleTheme.vue
+++ src/components/ToggleTheme/ToggleTheme.vue
@@ -1,50 +1,47 @@
 <script lang="ts" setup>
-import { useDark } from '@vueuse/core'
-import { nextTick } from 'vue'
+import { useDark } from "@vueuse/core";
+import { nextTick } from "vue";
 
-const isDark = useDark()
+const isDark = useDark();
 function toggleDark(event: MouseEvent) {
-  // TODO 解决startViewTransition类型问题
-  const isAppearanceTransition = (document as any).startViewTransition
-    && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+  const isAppearanceTransition =
+    Reflect.has(document, "startViewTransition") &&
+    !window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 
   if (!isAppearanceTransition) {
-    isDark.value = !isDark.value
-    return
+    isDark.value = !isDark.value;
+    return;
   }
 
-  const x = event.clientX
-  const y = event.clientY
+  const x = event.clientX;
+  const y = event.clientY;
   const endRadius = Math.hypot(
     Math.max(x, innerWidth - x),
-    Math.max(y, innerHeight - y),
-  )
+    Math.max(y, innerHeight - y)
+  );
 
   const transition = (document as any).startViewTransition(async () => {
-    isDark.value = !isDark.value
-    await nextTick()
-  })
-  transition.ready
-    .then(() => {
-      const clipPath = [
-        `circle(0px at ${x}px ${y}px)`,
-        `circle(${endRadius}px at ${x}px ${y}px)`,
-      ]
-      document.documentElement.animate(
-        {
-          clipPath: isDark.value
-            ? [...clipPath].reverse()
-            : clipPath,
-        },
-        {
-          duration: 400,
-          easing: 'ease-out',
-          pseudoElement: isDark.value
-            ? '::view-transition-old(root)'
-            : '::view-transition-new(root)',
-        },
-      )
-    })
+    isDark.value = !isDark.value;
+    await nextTick();
+  });
+  transition.ready.then(() => {
+    const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${endRadius}px at ${x}px ${y}px)`,
+    ];
+    document.documentElement.animate(
+      {
+        clipPath: isDark.value ? [...clipPath].reverse() : clipPath,
+      },
+      {
+        duration: 400,
+        easing: "ease-out",
+        pseudoElement: isDark.value
+          ? "::view-transition-old(root)"
+          : "::view-transition-new(root)",
+      }
+    );
+  });
 }
 </script>
 
